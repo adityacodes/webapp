28a955ad1562d0f52461f1dabedf7e5dcb7824d7
adityacodes <adityapadhi1996@gmail.com>
2017-03-14 17:03:59 +0530

    built layout for auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,236 +1,135 @@
+@extends('layouts.auth')
 
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="This is a online examination system">
-    <meta name="keywords" content="Exam system|exam|exams">
-    <link rel="icon" href="#/uploads/settings/I8GYi9XN7OrjAi2.png" type="image/x-icon">
-    <title> Dashboard</title>
-    <!-- Bootstrap Core CSS --> 
-    <link href="{{asset('user/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('user/css/bootstrap-datepicker.min.css')}}">
-    <link href="{{asset('user/css/sweetalert.css')}}" rel="stylesheet" type="text/css">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <!-- Morris Charts CSS --> 
-    <link href="{{asset('user/css/morris.css')}}" rel="stylesheet">
-    <!-- Custom CSS --> 
-    <link href="{{asset('user/css/sb-admin.css')}}" rel="stylesheet">
-    <!-- Custom Fonts --> 
-    <link href="{{asset('user/css/custom-fonts.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('user/css/materialdesignicons.css')}}" rel="stylesheet" type="text/css">
-    {{-- <link href="{{asset('font-awesome.min.css')}}" rel="stylesheet" type="text/css"> --}}
+@section('content')
+    <div class="login-content">
+        <div class="logo text-center"><img src="{{asset('img/gtp.png')}}" alt="" height="150" width="211">
+        <div class="text-center"><h2>Global Tech Promoters</h2></div>
+        </div>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --> 
-    
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// --> 
-        <!--[if lt IE 9]> 
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> 
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> 
-    <![endif]--> 
-</head>
-
-
-<body class="login-screen ng-scope" background="{{asset('img/lg-bg.jpg')}}" ng-app="academia" cz-shortcut-listen="true">
-
-<div class="login-content">
-    
-
-    <div class="logo text-center"><img src="{{asset('img/gtp.png')}}" alt="" height="150" width="211"></div>
-
-    <div class="menus">
-        <ul>
-            <li class="active">
-
-                <i class="icon-education-cap"></i>
-                Educate </li>
-                <li>
-
+        <div class="menus">
+            <ul>
+                <li class="active">
                     <i class="icon-education-cap"></i>
-                    Enlight
+                    Educate </li>
+                    <li>
+                        <i class="icon-education-cap"></i>
+                        Enlight
 
-                </li>
-                <li>
-
-                    <i class="icon-education-cap"></i>
-                    Enforce
-
-                </li>
-
-            </ul>
+                    </li>
+                    <li>
+                        <i class="icon-education-cap"></i>
+                        Enforce
+                    </li>
+                </ul>
         </div>  
+        <form method="POST" action="{{url('login')}}" accept-charset="UTF-8" name="loginForm" novalidate="" class="loginform ng-valid-minlength ng-dirty ng-valid-parse ng-valid ng-valid-required">
+                {{Form::token()}}
+
+                <div class="input-group">
+
+                    <span class="input-group-addon" id="basic-addon1">
+                        <i class="icon icon-user"></i>
+                    </span>
 
 
 
-        
+                    <input class="form-control ng-untouched ng-dirty ng-valid-parse ng-valid ng-valid-required" ng-model="email" required="true" id="email" placeholder="Username/Email" 
+                    ng-class="{&quot;has-error&quot;: loginForm.email.$touched &amp;&amp; loginForm.email.$invalid}" 
+                    name="email" type="text">
 
-        <form method="POST" action="#/login" accept-charset="UTF-8" name="loginForm" novalidate="" class="loginform ng-valid-minlength ng-dirty ng-valid-parse ng-valid ng-valid-required">
-            <input name="_token" type="hidden" value="{{csrf_token()}}">
-
-            <div class="input-group">
-
-                <span class="input-group-addon" id="basic-addon1">
-                    <i class="icon icon-user"></i>
-                </span>
-
-
-
-                <input class="form-control ng-untouched ng-dirty ng-valid-parse ng-valid ng-valid-required" ng-model="email" required="true" id="email" placeholder="Username/Email" 
-                ng-class="{&quot;has-error&quot;: loginForm.email.$touched &amp;&amp; loginForm.email.$invalid}" 
-                name="email" type="text">
-
-                <div class="validation-error ng-inactive" ng-messages="loginForm.email.$error">
-
-                    <!-- ngMessage: required -->
-
-                    <!-- ngMessage: email -->
-
-                </div>
-
-
-
-            </div>
-
-
-
-            <div class="input-group">
-
-                <span class="input-group-addon" id="basic-addon1"><i class="icon icon-lock"></i></span>
-
-                <input class="form-control instruction-call ng-untouched ng-valid-minlength ng-dirty ng-valid-parse ng-valid ng-valid-required" placeholder="Password" ng-model="registration.password" required="true" id="password" ng-class="{&quot;has-error&quot;: loginForm.password.$touched &amp;&amp; loginForm.password.$invalid}" ng-minlength="5" name="password" type="password" value="">
-
-                <div class="validation-error ng-inactive" ng-messages="loginForm.password.$error">
-
-                    <!-- ngMessage: required -->
-
-                    <!-- ngMessage: minlength -->
-
-                </div>
-
-
-
-            </div>
-
-            <div class="text-center buttons">
-
-                <button type="submit" id="submit_button" class="btn button btn-success btn-lg" ng-disabled="!loginForm.$valid">Login</button>
-
-                <div class="social-logins">
-                    <a href="#/auth/facebook" class="btn btn-lg btn-facebook btn-full"><i class="fa fa-facebook"></i> Login With Facebook</a>
-
-
-                    <a href="#/auth/google" class="btn btn-lg btn-google-plus btn-full"><i class="fa fa-google-plus"></i>  Login With Google</a>
-                    
-                    <div class="alert alert-info margintop30">
-                        <strong>Note: </strong>
-                        Social Logins Are Only For Student Accounts
+                    <div class="validation-error ng-inactive" ng-messages="loginForm.email.$error">
+                        <!-- ngMessage: required -->
+                        <!-- ngMessage: email -->
                     </div>
                 </div>
 
-            </div>
+                <div class="input-group">
 
+                    <span class="input-group-addon" id="basic-addon1"><i class="icon icon-lock"></i></span>
+
+                    <input class="form-control instruction-call ng-untouched ng-valid-minlength ng-dirty ng-valid-parse ng-valid ng-valid-required" placeholder="Password" ng-model="registration.password" required="true" id="password" ng-class="{&quot;has-error&quot;: loginForm.password.$touched &amp;&amp; loginForm.password.$invalid}" ng-minlength="5" name="password" type="password" value="">
+
+                    <div class="validation-error ng-inactive" ng-messages="loginForm.password.$error">
+
+                        <!-- ngMessage: required -->
+
+                        <!-- ngMessage: minlength -->
+
+                    </div>
+
+
+
+                </div>
+
+                <div class="text-center buttons">
+
+                    <button type="submit" id="submit_button" class="btn button btn-success btn-lg" ng-disabled="!loginForm.$valid">Login</button>
+
+                    <div class="social-logins">
+                        <a href="#/auth/facebook" class="btn btn-lg btn-facebook btn-full"><i class="fa fa-facebook"></i> Login With Facebook</a>
+
+
+                        <a href="#/auth/google" class="btn btn-lg btn-google-plus btn-full"><i class="fa fa-google-plus"></i>  Login With Google</a>
+                        
+                        <div class="alert alert-info margintop30">
+                            <strong>Note: </strong>
+                            Social Logins Are Only For Student Accounts
+                        </div>
+                    </div>
+
+                </div>
         </form>
 
-        
-
         <div class="footer">
-
             <a href="javascript:void(0);" class="pull-left" data-toggle="modal" data-target="#myModal"><i class="icon icon-question"></i> Forgot Password</a>
-
-
-
-            <a href="#/register" class="pull-right"><i class="icon icon-add-user"></i> Register</a>
-
+            <a href="{{url('register')}}" class="pull-right"><i class="icon icon-add-user"></i> Register</a>
         </div>
 
     </div>
-
-
 
     <!-- Modal -->
 
     <div id="myModal" class="modal fade" role="dialog">
-
         <div class="modal-dialog">
 
-            <form method="POST" action="#/users/forgot-password" accept-charset="UTF-8" name="passwordForm" novalidate="" class="loginform ng-pristine ng-valid-email ng-valid ng-valid-required"><input name="_token" type="hidden" value="XO9WIFSin6bS3EUNc7PqjVX7Mt4BrF1mPvCbMDrh">
+            <form method="POST" action="{{ url('password/reset') }}" accept-charset="UTF-8" name="passwordForm" novalidate="" class="loginform ng-pristine ng-valid-email ng-valid ng-valid-required">
+            {{Form::token()}}
 
                 <!-- Modal content-->
-
                 <div class="modal-content">
-
                     <div class="modal-header">
-
                         <button type="button" class="close" data-dismiss="modal">×</button>
-
                         <h4 class="modal-title">Forgot Password</h4>
-
                     </div>
-
                     <div class="modal-body">
-
                         <div class="input-group">
-
                             <span class="input-group-addon" id="basic-addon1"><i class="icon icon-email-resend"></i></span>
-
-
-
                             <input class="form-control ng-pristine ng-untouched ng-valid-email ng-valid ng-valid-required" ng-model="email" required="true" placeholder="Email" ng-class="{&quot;has-error&quot;: passwordForm.email.$touched &amp;&amp; passwordForm.email.$invalid}" name="email" type="email">
-
                             <div class="validation-error ng-inactive" ng-messages="passwordForm.email.$error">
-
                                 <!-- ngMessage: required -->
-
                                 <!-- ngMessage: email -->
-
                             </div>
-
-
-
                         </div>
-
                     </div>
 
                     <div class="modal-footer">
-
                         <div class="pull-right">
-
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-
                             <button type="submit" class="btn btn-primary" ng-disabled="!passwordForm.$valid">Submit</button>
-
                         </div>
-
                     </div>
-
                 </div>
 
             </form>
-
         </div>
-
     </div>
 
 
-<!-- jQuery --> 
-    <script async="" src="{{asset('user/js/analytics.js')}}"></script>
-    <script src="{{asset('user/js/jquery-1.12.1.min.js')}}"></script> 
 
-    <!-- Bootstrap Core JavaScript --> 
-    <script src="{{asset('user/js/bootstrap.min.js')}}"></script> 
-    <!--JS Control--> 
-    <script src="{{asset('user/js/main.js')}}"></script> 
-    <script src="{{asset('user/js/sweetalert-dev.js')}}"></script> 
-    <link rel="stylesheet" href="{{asset('user/css/alertify.core.css')}}">
-    <link rel="stylesheet" href="{{asset('user/css/alertify.default.css')}}" id="toggleCSS">
-    <script src="{{asset('user/js/alertify.js')}}"></script> 
-    <script type="text/javascript" src="{{asset('user/js/Chart.js')}}"></script> 
+@endsection
 
-    <script src="{{asset('user/js/angular.js')}}"></script>
-    <script src="{{asset('user/js/angular-messages.js')}}"></script> 
+@section('scripts') 
 
     <script>
 
@@ -365,5 +264,4 @@
             };
         app.directive("validImage", validImage);
             </script>
-</body>
-</html>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,82 +1,271 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('stylesheets')
+
+<style type="text/css">
+    /* Hidding the radiobuttons & checkboxes */
+    input[type="radio"], input[type="checkbox"] {
+        display: none;
+    }
+    /* Styling the "radio" status */
+    input[type="radio"] + label .radio-button{width: 28px; height: 28px; border: 2px solid #e1e8f8; border-radius: 100%;}
+    input[type="radio"] + label .active{display: none;}
+    input[type="radio"] + label .active{width: 24px; height: 24px; line-height: 24px; font-size: 14px; border-radius: 100%;text-align: center;}
+    input[type="radio"] + label:hover .active{display: block; color: #efefef;}
+    input[type="radio"]:checked + label .active{display: block; color: #fff; background: #44a1ef;}
+
+</style>
+
+
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {{ csrf_field() }}
+<div class="login-content">
+    <div class="logo text-center"><img src="{{asset('img/gtp.png')}}" alt="" height="150" width="211">
+        <div class="text-center"><h2>Global Tech Promoters</h2></div>
+    </div>
+    <div class="menus">
+        <ul>
+            <li class="active">
+                <i class="icon-education-cap"></i>
+                Educate </li>
+                <li>
+                    <i class="icon-education-cap"></i>
+                    Enlight
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i> Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                </li>
+                <li>
+                    <i class="icon-education-cap"></i>
+                    Enforce
+                </li>
+            </ul>
+        </div> 
+        <form method="POST" action="{{url('register')}}" accept-charset="UTF-8" name="registrationForm" novalidate="" class="loginform">
+            {{Form::token()}}
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon1"><i class="icon icon-user"></i></span>
+                <input class="form-control" placeholder="Name" ng-model="name" ng-pattern="/^[a-zA-Z0-9_&#039;.- ]*$/" required="true" ng-class="{&quot;has-error&quot;: registrationForm.name.$touched &amp;&amp; registrationForm.name.$invalid}" ng-minlength="4" name="name" type="text">
+                <div class="validation-error" ng-messages="registrationForm.name.$error" >
+                    <p ng-message="required">This Field Is Required</p>
+                    <p ng-message="minlength">The Text Is Too Short</p>
+                    <p ng-message="pattern">Invalid Input</p>
                 </div>
             </div>
+
+
+
+            <div class="input-group">
+
+                <span class="input-group-addon" id="basic-addon1"><i class="icon icon-user"></i></span>
+
+
+
+                <input class="form-control" placeholder="Username" ng-model="username" required="true" ng-class="{&quot;has-error&quot;: registrationForm.username.$touched &amp;&amp; registrationForm.username.$invalid}" ng-minlength="4" name="username" type="text">
+
+                <div class="validation-error" ng-messages="registrationForm.username.$error" >
+
+                    <p ng-message="required">This Field Is Required</p>
+
+                    <p ng-message="minlength">The Text Is Too Short</p>
+
+                    <p ng-message="pattern">Invalid Input</p>
+
+                </div>
+            </div>
+
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon1"><i class="icon icon-email-resend"></i></span>
+                <input class="form-control" placeholder="Email" ng-model="email" required="true" 
+                        ng-class="{&quot;has-error&quot;: registrationForm.email.$touched &amp;&amp; registrationForm.email.$invalid}" name="email" type="email">
+                <div class="validation-error" ng-messages="registrationForm.email.$error" >
+                    <p ng-message="required">This Field Is Required</p>
+                    <p ng-message="email">Please Enter Valid Email</p>
+                </div>
+            </div>
+
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon1"><i class="icon icon-lock"></i></span>
+
+                <input class="form-control instruction-call" placeholder="Password" ng-model="registration.password" required="true" ng-class="{&quot;has-error&quot;: registrationForm.password.$touched &amp;&amp; registrationForm.password.$invalid}" ng-minlength="5" name="password" type="password" value="">
+
+                <div class="validation-error" ng-messages="registrationForm.password.$error" >
+                    <p ng-message="required">This Field Is Required</p>
+                    <p ng-message="minlength">The Password Is Too Short</p>
+                </div>
+            </div>
+
+
+
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon1"><i class="icon icon-lock"></i></span>
+                <input class="form-control instruction-call" placeholder="Password Confirmation" ng-model="registration.password_confirmation" required="true" ng-class="{&quot;has-error&quot;: registrationForm.password_confirmation.$touched &amp;&amp; registrationForm.password_confirmation.$invalid}" ng-minlength="5" compare-to="registration.password" name="password_confirmation" type="password" value="">
+                <div class="validation-error" ng-messages="registrationForm.password_confirmation.$error" >
+                    <p ng-message="required">This Field Is Required</p>
+                    <p ng-message="minlength">The Text Is Too Short</p>
+                    <p ng-message="compareTo">Password And Confirm Password Does Not Match</p>
+                </div>
+            </div>
+            <br>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <input id="free" checked="checked" name="is_student" type="radio" value="0">
+
+                    <label for="free"> <span class="fa-stack radio-button"> <i class="mdi mdi-check active"></i> </span> I Am A Student</label> 
+                </div>
+
+                <div class="col-md-6">
+
+                    <input id="paid" name="is_student" type="radio" value="1">
+
+                    <label for="paid"> 
+
+                        <span class="fa-stack radio-button"> <i class="mdi mdi-check active"></i> </span> I Am A Cyber Security Expert </label>
+                    </div>
+                </div>
+                <div class="text-center buttons">
+                    <button type="submit"  class="btn button btn-success btn-lg" 
+                    ng-disabled='!registrationForm.$valid'>Register Now</button>
+                </div>
+            </form>
+            <a href="{{url('login')}}"><p class="text-center">I Am Having Account </a></p>
         </div>
-    </div>
-</div>
+
+
+        @endsection 
+        @section('scripts')
+
+        <script >
+
+           app = angular.module('academia', ['ngMessages']);
+           app.directive('stringToNumber', function() {
+              return {
+                require: 'ngModel',
+                link: function(scope, element, attrs, ngModel) {
+                  ngModel.$parsers.push(function(value) {
+                    return '' + value;
+                });
+                  ngModel.$formatters.push(function(value) {
+                    return parseFloat(value);
+                });
+              }
+          };
+      });
+           app.directive('input', function ($parse) {
+              return {
+                restrict: 'E',
+                multiElement: true,
+                require: '?ngModel',
+                link: function (scope, element, attrs) {
+                   e = element[0];
+
+                   if (attrs.ngModel && attrs.value) {
+                    $parse(attrs.ngModel).assign(scope, attrs.value);
+                }
+            }
+        };
+    });
+
+           app.directive('textarea', function ($parse) {
+              return {
+                restrict: 'E',
+                multiElement: true,
+                require: '?ngModel',
+                link: function (scope, element, attrs) {
+                    e = element[0];
+             // console.log(element[0].value);
+             if (attrs.ngModel && e.value) {
+                $parse(attrs.ngModel).assign(scope, e.value);
+            }
+        }
+    };
+});
+           app.directive('select', function ($parse) {
+              return {
+                restrict: 'E',
+                multiElement: true,
+                require: '?ngModel',
+                link: function (scope, element, attrs) {
+                    e = element[0];
+                    selectedValue = e.options[e.selectedIndex].value;
+                // console.log(selectedValue);
+                if (attrs.ngModel && selectedValue) {
+                    $parse(attrs.ngModel).assign(scope, selectedValue);
+                }
+            }
+        };
+    });
+
+/**
+ * PASSWORD AND CONFIRM PASSWORD FIELDS VALIDATION DIRECTIVE
+ * @return  {[type]} [description]
+ */
+ var compareTo = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+
+app.directive("compareTo", compareTo);
+
+/**
+ * FILE VALIDATION DIRECTIVES BEFORE UPLOAD
+ */
+
+ var validImage = function($rootScope) {
+    var validFormats = ['jpg', 'png', 'jpeg'];
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=validImage"
+        },
+        link: function(scope, element, attributes, ngModel) {
+            $rootScope.isImageValid = 'true';
+            // console.log(element);
+            ngModel.$validators.validImage = function(modelValue) {
+
+             element.on('change', function () {
+
+                // e = element[0];
+                // fileObject = $.parseJSON(e.files);
+                // console.log(e.files);
+                file_size = this.files[0].size;
+                console.log(file_size);
+                valid_size = true;
+                
+                var value = element.val(),
+                ext = value.substring(value.lastIndexOf('.') + 1).toLowerCase();   
+                console.log(validFormats.indexOf(ext) !== -1);
+                $rootScope.isImageValid = validFormats.indexOf(ext) !== -1;
+                $rootScope.$apply();
+                console.log( " $rootScope.isImageValid   "+$rootScope.isImageValid);
+                if(file_size > 2000000){
+
+                    return false;
+                }
+                else{
+                   return validFormats.indexOf(ext) !== -1 ;
+               }
+           });
+         };
+
+
+     }
+ };
+};
+app.directive("validImage", validImage);
+</script>;
 @endsection
